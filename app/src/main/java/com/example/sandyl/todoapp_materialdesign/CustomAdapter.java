f7209c9edb100d9deb1e8a857427b88dc3895eb8
Sandy L <sandyl@Sandys-MacBook-Pro-2.local>
2017-02-18 17:47:51 -0500

    created custom row and adapter

---
+++ app/src/main/java/com/example/sandyl/todoapp_materialdesign/CustomAdapter.java
@@ -0,0 +1,27 @@
+package com.example.sandyl.todoapp_materialdesign;
+
+import android.support.v7.widget.RecyclerView;
+import android.view.ViewGroup;
+
+/**
+ * Created by sandyl on 2017-02-18.
+ */
+
+public class CustomAdapter extends RecyclerView.Adapter {
+
+
+    @Override
+    public RecyclerView.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        return null;
+    }
+
+    @Override
+    public void onBindViewHolder(RecyclerView.ViewHolder holder, int position) {
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return 0;
+    }
+}
