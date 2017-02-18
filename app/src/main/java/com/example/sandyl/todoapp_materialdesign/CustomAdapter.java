6763b31030d7c29712a258a648a2c04568e81811
Sandy L <sandyl@Sandys-MacBook-Pro-2.local>
2017-02-18 18:09:50 -0500

    custom adapter methods and new class Todo with 2 properties

--- app/src/main/java/com/example/sandyl/todoapp_materialdesign/CustomAdapter.java
+++ app/src/main/java/com/example/sandyl/todoapp_materialdesign/CustomAdapter.java
@@ -1,7 +1,9 @@
 package com.example.sandyl.todoapp_materialdesign;
 
 
+import android.content.Context;
 import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
@@ -12,14 +14,26 @@ import android.widget.TextView;
 
 public class CustomAdapter extends RecyclerView.Adapter<CustomAdapter.ItemViewHolder> {
 
+    private LayoutInflater inflater;
+
+
+    public CustomAdapter(Context context) {
+        inflater = LayoutInflater.from(context);
+    }
 
     @Override
     public ItemViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
-        return null;
+        View view = inflater.inflate(R.layout.custom_row, parent, false);
+        ItemViewHolder itemsHolder = new ItemViewHolder(view);
+        return itemsHolder;
     }
 
     @Override
     public void onBindViewHolder(ItemViewHolder holder, int position) {
+
+        Todo currentTodo = new Todo();
+        holder.taskView.setText(currentTodo.text);
+        holder.priorityTextView.setText(currentTodo.priority);
 
     }
 
@@ -39,7 +53,7 @@ public class CustomAdapter extends RecyclerView.Adapter<CustomAdapter.ItemViewHo
             super(itemView);
 
             taskView = (TextView) itemView.findViewById(R.id.taskTextView);
-            priorityTextView = (TextView) itemView.findViewById(R.id.priorityTextView)
+            priorityTextView = (TextView) itemView.findViewById(R.id.priorityTextView);
         }
     }
 
---
+++ app/src/main/java/com/example/sandyl/todoapp_materialdesign/Todo.java
@@ -0,0 +1,18 @@
+package com.example.sandyl.todoapp_materialdesign;
+
+/**
+ * Created by sandyl on 2017-02-18.
+ */
+
+public class Todo {
+
+    String text;
+    String priority;
+
+    public Todo() {}
+
+    public Todo(String text, String priority) {
+        this.text = text;
+        this.priority = priority;
+    }
+}
