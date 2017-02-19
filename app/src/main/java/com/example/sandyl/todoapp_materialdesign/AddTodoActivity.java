84a8b9d065c78b2cedb9804b1d027fef58cb72ef
Sandy L <sandyl@Sandys-MacBook-Pro-2.local>
2017-02-18 20:53:50 -0500

    additional properties added to Todo Class, new Status enum, menu added to toolbar with actions to save and dismiss current activity, addTodo() to add new todo to list

--- app/src/main/java/com/example/sandyl/todoapp_materialdesign/AddTodoActivity.java
+++ app/src/main/java/com/example/sandyl/todoapp_materialdesign/AddTodoActivity.java
@@ -1,8 +1,12 @@
 package com.example.sandyl.todoapp_materialdesign;
 
 import android.app.DatePickerDialog;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.DatePicker;
@@ -14,7 +18,7 @@ import java.util.Calendar;
 
 public class AddTodoActivity extends AppCompatActivity {
 
-
+    private Toolbar mToolbar;
     final Calendar myCalendar = Calendar.getInstance();
     Button addButton;
     EditText todoEditText;
@@ -28,6 +32,43 @@ public class AddTodoActivity extends AppCompatActivity {
     String status;
 
 
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.add_todo_menu, menu);
+
+
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        switch (id) {
+            case R.id.action_save:
+
+                saveTask();
+                finish();
+                return true;
+
+            case R.id.action_dismiss:
+
+                finish();
+                return true;
+
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+
+    }
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -37,6 +78,11 @@ public class AddTodoActivity extends AppCompatActivity {
         dateTextView = (TextView) findViewById(R.id.dateTextView);
         priorityRadioGroup = (RadioGroup) findViewById(R.id.priorityRadioGroup);
 
+
+        mToolbar = (Toolbar) findViewById(R.id.toolbar);
+
+        setSupportActionBar(mToolbar);
+        getSupportActionBar().setDisplayShowHomeEnabled(true);
 
         addButton = (Button) findViewById(R.id.addButton);
 
@@ -52,10 +98,6 @@ public class AddTodoActivity extends AppCompatActivity {
     }
 
 
-
-
-
-
     final DatePickerDialog.OnDateSetListener date = new DatePickerDialog.OnDateSetListener() {
         @Override
         public void onDateSet(DatePicker view, int year, int monthOfYear,
@@ -66,4 +108,12 @@ public class AddTodoActivity extends AppCompatActivity {
 
         }
     };
+
+    public void saveTask() {
+
+        Intent intent = new Intent();
+        intent.putExtra("task", todoEditText.getText().toString());
+        setResult(1, intent);
+
+    }
 }
--- app/src/main/java/com/example/sandyl/todoapp_materialdesign/MainActivity.java
+++ app/src/main/java/com/example/sandyl/todoapp_materialdesign/MainActivity.java
@@ -6,15 +6,18 @@ import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.Toolbar;
 import android.util.Log;
 import android.view.View;
 
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
     private String TAG;
+    private Toolbar mToolbar;
     RecyclerView recyclerView;
     List<Todo> todos;
     CustomAdapter adapter;
@@ -24,6 +27,11 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mToolbar = (Toolbar) findViewById(R.id.toolbar);
+
+        setSupportActionBar(mToolbar);
+        getSupportActionBar().setDisplayShowHomeEnabled(true);
 
         recyclerView = (RecyclerView) findViewById(R.id.rvItems);
         todos = new ArrayList<Todo>();
@@ -62,6 +70,20 @@ public class MainActivity extends AppCompatActivity {
         return data;
     }
 
+
+    public void addTodo(String text, Date date, Todo.Status status, Todo.Priority priority) {
+
+        Todo newTodo = new Todo();
+        newTodo.text = text;
+        newTodo.date = date;
+        newTodo.status = status;
+        newTodo.priority = priority;
+
+        todos.add(newTodo);
+        adapter.notifyDataSetChanged();
+
+    }
+
     public void AddTodoAction() {
 
         fab = (FloatingActionButton) findViewById(R.id.fab);
@@ -69,9 +91,21 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View view) {
                 Intent intent = new Intent(MainActivity.this, AddTodoActivity.class);
-                startActivity(intent);
+                startActivityForResult(intent, 1);
             }
         });
+
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+
+        String text = data.getStringExtra("task");
+
+        //add todo to list on saving
+        addTodo(text, new Date(), Todo.Status.ACTIVE, Todo.Priority.MEDIUM);
 
     }
 }
