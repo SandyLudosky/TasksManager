cc7e7460cc3d3726ca9f193af57f4598b8aec697
Sandy L <sandyl@Sandys-MacBook-Pro-2.local>
2017-02-18 20:21:38 -0500

    added ui on AddTodoActivity, DatePickerDialog on button click

--- app/src/main/java/com/example/sandyl/todoapp_materialdesign/AddTodoActivity.java
+++ app/src/main/java/com/example/sandyl/todoapp_materialdesign/AddTodoActivity.java
@@ -1,13 +1,52 @@
 package com.example.sandyl.todoapp_materialdesign;
 
-import android.support.v7.app.AppCompatActivity;
+import android.app.DatePickerDialog;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.DatePicker;
+
+import java.util.Calendar;
 
 public class AddTodoActivity extends AppCompatActivity {
+
+
+    final Calendar myCalendar = Calendar.getInstance();
+    Button addButton;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_todo);
+
+        addButton = (Button) findViewById(R.id.addButton);
+
+        addButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                new DatePickerDialog(AddTodoActivity.this, date, myCalendar
+                        .get(Calendar.YEAR), myCalendar.get(Calendar.MONTH),
+                        myCalendar.get(Calendar.DAY_OF_MONTH)).show();
+            }
+        });
+
     }
+
+
+
+
+
+
+    final DatePickerDialog.OnDateSetListener date = new DatePickerDialog.OnDateSetListener() {
+        @Override
+        public void onDateSet(DatePicker view, int year, int monthOfYear,
+                              int dayOfMonth) {
+            myCalendar.set(Calendar.YEAR, year);
+            myCalendar.set(Calendar.MONTH, monthOfYear);
+            myCalendar.set(Calendar.DAY_OF_MONTH, dayOfMonth);
+
+        }
+    };
 }
