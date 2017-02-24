cce10e084e8f7103d60afe52aa8e63bd98b44ba6
Sandy L <sandyl.udemy@gmail.com>
2017-02-23 20:11:46 -0500

    added animation to splash screen

--- app/src/main/java/com/example/sandyl/todoapp_materialdesign/SplashScreen.java
+++ app/src/main/java/com/example/sandyl/todoapp_materialdesign/SplashScreen.java
@@ -4,12 +4,20 @@ import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
-import android.view.Menu;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
 
 public class SplashScreen extends Activity {
 
+    interface MyCallback {
+        void callbackCall();
+    }
+
+
     /** Duration of wait **/
-    private final int SPLASH_DISPLAY_LENGTH = 1000;
+    private final int SPLASH_DISPLAY_LENGTH = 2000;
+    private final int ANIMATION_DELAY_LENGTH = 1200;
 
     /** Called when the activity is first created. */
     @Override
@@ -28,5 +36,27 @@ public class SplashScreen extends Activity {
                 finish();
             }
         }, SPLASH_DISPLAY_LENGTH);
+
+
+        new Handler().postDelayed(new Runnable(){
+            @Override
+            public void run() {
+                /* Create an Intent that will start the Menu-Activity. */
+                AnimateZoom();
+            }
+        }, ANIMATION_DELAY_LENGTH);
+
     }
+
+    public void AnimateZoom(){
+        ImageView image = (ImageView)findViewById(R.id.imageView);
+        Animation animation1 = AnimationUtils.loadAnimation(getApplicationContext(),
+                R.anim.anim_zoom_in);
+        Animation animation2 = AnimationUtils.loadAnimation(getApplicationContext(),
+                R.anim.anim_zoom_out);
+        image.startAnimation(animation1);
+        image.startAnimation(animation2);
+
+    }
+
 }
